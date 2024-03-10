b648ff11840f35072a21a8c665f3ee5a4bc262e3
Giorgi Chikviladze <chikviladze555@yahoo.com>
2024-03-10 15:33:59 +0400

    change from developer to production

--- server/config/auth/passportAuth.js
+++ server/config/auth/passportAuth.js
@@ -1,24 +1,24 @@
-require("dotenv").config();
-const passport = require("../../config/passport");
-const AdditionUserInfo = require("../../models/AdditionUserInfoSchema");
+require('dotenv').config();
+const passport = require('../../config/passport');
+const AdditionUserInfo = require('../../models/AdditionUserInfoSchema');
 
 // Middleware to authenticate using Google OAuth with specified scope
-const auth = passport.authenticate("google", { scope: ["email", "profile"] });
+const auth = passport.authenticate('google', { scope: ['email', 'profile'] });
 
-const DEV_MODE = process.env.NODE_ENV === "production";
+const DEV_MODE = process.env.NODE_ENV !== 'production';
 
 // Middleware to handle callback from Google OAuth authentication
-const callbackAuth = passport.authenticate("google", {
+const callbackAuth = passport.authenticate('google', {
   failureRedirect: DEV_MODE
-    ? "https://brain-house.vercel.app"
-    : "http://localhost:5173", // Redirect to home if authentication fails
+    ? 'https://brain-house.vercel.app'
+    : 'http://localhost:5173', // Redirect to home if authentication fails
 });
 
-const fbAuth = passport.authenticate("facebook", { scope: ["email"] });
-const fbCallbackAuth = passport.authenticate("facebook", {
+const fbAuth = passport.authenticate('facebook', { scope: ['email'] });
+const fbCallbackAuth = passport.authenticate('facebook', {
   failureRedirect: DEV_MODE
-    ? "https://brain-house.vercel.app"
-    : "http://localhost:5173", // Redirect to home if authentication fails
+    ? 'https://brain-house.vercel.app'
+    : 'http://localhost:5173', // Redirect to home if authentication fails
 });
 
 // Redirect middleware after successful authentication
@@ -30,14 +30,14 @@ const redirect = async (req, res) => {
   if (userInfo.length > 0) {
     // User is registered, redirect to a route of your choice
     res.redirect(
-      DEV_MODE ? "https://brain-house.vercel.app" : "http://localhost:5173"
+      DEV_MODE ? 'https://brain-house.vercel.app' : 'http://localhost:5173'
     );
   } else {
     // User is not registered, redirect to the registration route
     res.redirect(
       DEV_MODE
-        ? "https://brain-house.vercel.app/register"
-        : "http://localhost:5173/register"
+        ? 'https://brain-house.vercel.app/register'
+        : 'http://localhost:5173/register'
     );
   }
 };
