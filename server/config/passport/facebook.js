c19bb0868ce20ec2657bbf982f5a7f30aec60cb3
Giorgi Chikviladze <chikviladze555@yahoo.com>
2024-03-10 21:40:08 +0400

    fix errors

--- server/config/passport/facebook.js
+++ server/config/passport/facebook.js
@@ -2,43 +2,46 @@ const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const User = require('../../models/userSchema');
 
-const facebookStrategy = new FacebookStrategy(
-  {
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: 'https://brain-house-vkk7.onrender.com/auth/facebook/callback',
-    profileFields: ['id', 'emails', 'name'],
-  },
-  async function (accessToken, refreshToken, profile, cb) {
-    try {
-      if (!profile.emails || profile.emails.length === 0) {
-        return cb(new Error('Email not provided in the profile'), null);
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL:
+        'https://brain-house-vkk7.onrender.com/auth/facebook/callback',
+      profileFields: ['id', 'emails', 'name'],
+    },
+    async function (accessToken, refreshToken, profile, cb) {
+      try {
+        if (!profile.emails || profile.emails.length === 0) {
+          return cb(new Error('Email not provided in the profile'), null);
+        }
+
+        const email = profile.emails[0].value;
+
+        let user = await User.findOne({ email });
+        console.log('facebook profile', profile);
+        console.log('if facebook user exists', user);
+
+        if (!user) {
+          user = await User.create({
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            email: email,
+            provider: 'facebook', // Set the provider property
+          });
+          console.log('if facebook user does not exist', user);
+        }
+        return cb(null, user);
+      } catch (error) {
+        return cb(error, null);
       }
-
-      const email = profile.emails[0].value;
-
-      let user = await User.findOne({ email });
-      console.log('facebook profile', profile);
-      console.log('if facebook user exists', user);
-
-      if (!user) {
-        user = await User.create({
-          firstName: profile.name.givenName,
-          lastName: profile.name.familyName,
-          email: email,
-          provider: 'facebook', // Set the provider property
-        });
-        console.log('if facebook user does not exist', user);
-      }
-      return cb(null, user);
-    } catch (error) {
-      return cb(error, null);
     }
-  }
+  )
 );
 
 passport.serializeUser(function (user, done) {
-  console.log('serialized user', user);
+  console.log('serialized facebook user', user);
 
   done(null, user.id);
 });
@@ -46,11 +49,11 @@ passport.serializeUser(function (user, done) {
 passport.deserializeUser(async function (id, done) {
   try {
     const user = await User.findById(id);
-    console.log('deserialized user', user);
+    console.log('deserialized facebook user', user);
     done(null, user);
   } catch (error) {
     done(error, null);
   }
 });
 
-module.exports = facebookStrategy;
+module.exports = passport;
