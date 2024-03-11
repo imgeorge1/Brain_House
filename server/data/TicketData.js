da26ecf196ae9a19dec0cf6a159be5d2b032ec81
User <Ubitoz133@gmail.com>
2024-03-11 12:36:28 +0400

    duplicate

--- server/data/TicketData.js
+++ server/data/TicketData.js
@@ -5073,6 +5073,13 @@ const largeTicketList = [
     correctAnswer: 2,
     quantityAnswer: 3,
   },
+  {
+    id: 908,
+    categoryID: 21,
+    image: "",
+    correctAnswer: 2,
+    quantityAnswer: 3,
+  },
   //
 ];
 
