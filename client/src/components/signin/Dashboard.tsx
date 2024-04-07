7c67b73afaf559d1d5cc76b8f041032b799aca10
User <Ubitoz133@gmail.com>
2024-04-07 13:00:18 +0400

    changed category logo and improved swiper responsiveness

--- client/src/components/signin/Dashboard.tsx
+++ client/src/components/signin/Dashboard.tsx
@@ -12,7 +12,7 @@ const Dashboard = ({ currentUser, setShow }: DashboardTypes) => {
     <div>
       {currentUser ? (
         <div className="px-1 gap-2 justify-end sign-in-parent">
-          {checkAdmin && (
+          {/* {checkAdmin && (
             <Link
               to="dashboard"
               className="ctrlbtn bg-green-500 p-2 rounded-2xl text-xl font-bold text-white"
@@ -20,10 +20,12 @@ const Dashboard = ({ currentUser, setShow }: DashboardTypes) => {
             >
               Dashboard
             </Link>
-          )}
+          )} */}
 
           <h4 className="ctrlbtn mt-auto mb-auto users-name">
-            {currentUser.firstName + " " + (currentUser.lastName || "")}
+            {currentUser.firstName +
+              " " +
+              (currentUser.lastName || "Zurabishvili")}
           </h4>
           <button
             className="sign-out ctrlbtn buttonBorder px-6 py-2 rounded-3xl duration-200 hover:bg-orange-500 text-xl text-white"
