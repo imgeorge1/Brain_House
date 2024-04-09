f0e4b8576dedb2956247f7cd3dcd64fed6a9ffda
User <Ubitoz133@gmail.com>
2024-04-09 21:06:16 +0400

    improved main page

--- client/src/components/main/Main.tsx
+++ client/src/components/main/Main.tsx
@@ -5,14 +5,9 @@ const Main = () => {
   return (
     <main className="w-full mt-4 items-center px-4 md:px-20">
       <div className="flex flex-col md:flex-row justify-evenly gap-3">
-        <img
-          className="w-full h-full md:max-w-[400px]"
-          src={BrainHouse}
-          alt="Brain House logo"
-        />
         <div className="">
-          <h2 className="text-4xl font-bold">რა არის ბრეინ ჰაუსი?</h2>
-          <p className="text-2xl mt-2 max-w-[550px]">
+          <h2 className="text-4xl font-bold main-text">რა არის ბრეინ ჰაუსი?</h2>
+          <p className="text-xl mt-8 max-w-[550px] main-desc">
             &nbsp; ბრეინ ჰაუსი არის სასწავლო ცენტრი, სადაც სახლიდან გაუსვლელად
             და მარტივად შეგიძლიათ უცხო ენის შესწავლა. ჩვენთან შეგიძლიათ:
             ინგლისური, რუსული, ჩინური, გერმანული და ფრანგული ენების შესწავლა!
@@ -21,21 +16,34 @@ const Main = () => {
             მიერ.
           </p>
         </div>
+        <img
+          className="w-full h-full md:max-w-[350px] main-img"
+          src={BrainHouse}
+          alt="Brain House logo"
+        />
       </div>
       <div className="flex flex-col md:flex-row justify-evenly mt-8">
         <div className="">
-          <h2 className="text-4xl font-bold">ბრეინ ჰაუსში ...</h2>
-          <ul className="list-disc text-2xl p-6 max-w-[550px]">
-            <li>სწავლა ადვილია!</li>
-            <li>
+          <h2 className="text-4xl font-bold main-text">ბრეინ ჰაუსში ...</h2>
+          <ul className=" text-xl mt-2 p-6 max-w-[550px]">
+            <li className="li-item">სწავლა ადვილია!</li>
+            <li className="li-item">
               სწავლა შესაძლებელია ნებისმიერი ქვეყნიდან, ქალაქიდან ან სოფლიდან!
             </li>
-            <li>მოსწავლე შეიძლება იყოს 13-დან 60 წლამდე!</li>
-            <li>სწავლა ყველასთვის ხელმისაწვდომია!</li>
-            <li>მოსწავლეებს აქვთ საშინაო დავალებები და ტესტები!</li>
+            <li className="li-item">
+              მოსწავლე შეიძლება იყოს 13-დან 60 წლამდე!
+            </li>
+            <li className="li-item">სწავლა ყველასთვის ხელმისაწვდომია!</li>
+            <li className="li-item">
+              მოსწავლეებს აქვთ საშინაო დავალებები და ტესტები!
+            </li>
           </ul>
         </div>
-        <img src={Light} alt="newlogo" className="max-w-[400px] h-full" />
+        <img
+          src={Light}
+          alt="newlogo"
+          className="md:max-w-[350px] h-full main-img"
+        />
       </div>
     </main>
   );
