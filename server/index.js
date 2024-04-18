9320da2c7b4efa7ce86a2b618849f0b4c1257e24
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2024-04-18 23:47:42 +0400

    set false cookies secure for https

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
-      secure: true, // Set to true for HTTPS environments
+      secure: false, // Set to true for HTTPS environments
     },
     // store: MongoStore.create({
     //   mongoUrl: process.env.MONGODB_URL,
