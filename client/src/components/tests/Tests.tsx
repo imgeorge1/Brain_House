6ca09eef49f34ae4df672d1d214ce468e24cbe7f
User <Ubitoz133@gmail.com>
2024-04-20 21:43:14 +0400

    improved design on Tests page and signInModal

--- client/src/components/tests/Tests.tsx
+++ client/src/components/tests/Tests.tsx
@@ -33,15 +33,6 @@ const Tests = ({
 
   return (
     <div className=" mx-auto lg:max-w-[800px] mb-40 mt-28">
-      {location.pathname.split("/")[1] === "exams" &&
-        !start &&
-        passedQuestionLength !== 30 && (
-          <>
-            <p className="text-3xl">{currentTicketIndex + 1} / 30</p>
-            <p className="text-3xl">შეცდომა {countAnswer.incorrect}</p>
-            <p className="text-3xl">სწორი {countAnswer.correct}</p>
-          </>
-        )}
       {currentTicketIndex < ticketData.length && (
         <Ticket
           key={ticketData[currentTicketIndex].id}
@@ -51,22 +42,53 @@ const Tests = ({
           getAnswerClass={getAnswerClass}
         />
       )}
+      {location.pathname.split("/")[1] === "exams" &&
+        !start &&
+        passedQuestionLength !== 30 && (
+          <>
+            <div className="flex justify-around ">
+              <p
+                className="bg-[#babfc7] p-2 rounded-md text-lg mt-8 md:mt-5 md:text-2xl font-roboto
+               font-bold"
+              >
+                {currentTicketIndex + 1} / 30
+              </p>
+              <p
+                className="bg-red-500 p-2 rounded-md text-lg mt-8 md:mt-5 md:text-2xl font-roboto
+              font-bold"
+              >
+                შეცდომა: {countAnswer.incorrect}
+              </p>
+              <p
+                className="bg-green-500 p-2 rounded-md text-lg mt-8 md:mt-5 md:text-2xl font-roboto
+              font-bold"
+              >
+                სწორი: {countAnswer.correct}
+              </p>
+            </div>
+          </>
+        )}
       {passedQuestionLength === 30 && (
-        <div className="text-center text-3xl font-semibold mb-20 mt-80">
+        <div className="text-center text-3xl font-semibold mb-20 mt-80 font-roboto">
           <motion.h1
             initial={{ opacity: 0, y: -200 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
           >
-            თქვენ გქონდათ {countAnswer.incorrect} შეცდომა და დააგროვეთ{" "}
-            {countAnswer.correct} სწორი პასუხი
+            თქვენ გქონდათ{" "}
+            <span className="text-red-500">
+              {countAnswer.incorrect} შეცდომა
+            </span>{" "}
+            და დააგროვეთ{" "}
+            <span className="text-green-500">{countAnswer.correct} სწორი</span>{" "}
+            პასუხი
           </motion.h1>
           <motion.button
             initial={{ opacity: 0, rotate: 180 }}
             animate={{ opacity: 1, rotate: 0 }}
             transition={{ duration: 0.8 }}
             onClick={restartTest}
-            className="mt-14 px-2 bg-black text-white"
+            className="mt-14 p-2 bg-[#272559] text-white"
           >
             თავიდან დაწყება
           </motion.button>
