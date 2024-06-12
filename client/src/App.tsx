eae3538096ed69fb14d291f49e671ec0f2f7b6ee
unknown <chikviladze555@yahoo.com>
2024-06-12 15:01:31 +0400

    fix errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, Suspense, lazy } from "react";
+import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
