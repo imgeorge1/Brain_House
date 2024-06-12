5a4f28e2fb8a33c15cf41a53baf9f01700022796
unknown <chikviladze555@yahoo.com>
2024-06-12 16:53:10 +0400

    add iban and recipient

--- client/src/components/register/SignUp.tsx
+++ client/src/components/register/SignUp.tsx
@@ -140,13 +140,27 @@ const SignUp = ({
           >
             ანგარიშის ნომერი
           </label>
-          <input
-            type="text"
-            id="age"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
-            readOnly
-            value="010010908323123"
-          />
+          <div className="flex items-center justify-start pl-2 gap-2  ">
+            <span style={{ fontSize: "15px" }}>TBC ბანკი</span>{" "}
+            <input
+              type="text"
+              id="iban"
+              className="shadow appearance-none border rounded w-9/12 py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
+              readOnly
+              value="GE26TB7765836020100010"
+            />
+          </div>
+          <div className="flex items-center justify-start gap-5 pl-2 mt-2 ">
+            <span style={{ fontSize: "15px" }}>მიმღები</span>{" "}
+            <input
+              type="text"
+              id="recipient"
+              className="shadow appearance-none border rounded w-9/12 py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
+              readOnly
+              value="შ.პ.ს. ბრეინ ჰაუსი 2021"
+            />
+          </div>
+
           <p className="text-red-500 mt-2" style={{ fontSize: "12px" }}>
             ვიდეო ლექციებზე წვდომა მოგეცემათ თანხის ანგარიშზე ასახვის შემდეგ
           </p>
