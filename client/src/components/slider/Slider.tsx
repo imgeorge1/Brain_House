5f8b9efd220a60455bd9059826779a28598d937d
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2024-07-21 13:50:12 +0400

    change a tag with link tag and disable exam button

--- client/src/components/slider/Slider.tsx
+++ client/src/components/slider/Slider.tsx
@@ -5,6 +5,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "../../index.css";
 import { Navigation } from "swiper/modules";
+import { Link } from "react-router-dom";
 
 function Slider() {
   return (
@@ -28,12 +29,12 @@ function Slider() {
             <p className="text-1xl md:text-2xl text-[#FDE047] mt-2 font-roboto">
               მართვის მოწმობის გაკვეთილები
             </p>
-            <a href="/tickets/21">
+            <Link to="/tickets/21">
               <button className="font-roboto flex items-center justify-between w-[12rem] px-4 py-2 md:w-60 md:px-7 md:py-3 rounded-2xl bg-[#2D2862] text-xl  md:text-2xl text-white slider-button">
                 გაიგეთ მეტი
                 <FaArrowRight />
               </button>
-            </a>
+            </Link>
           </div>
         </div>
       </SwiperSlide>
@@ -54,12 +55,12 @@ function Slider() {
             <p className="text-1xl md:text-2xl text-[#FDE047] mt-2 font-roboto">
               ჩვენ ვასწავლით ონლაინ, უმარტივესი მეთოდებით
             </p>
-            <a href="/about">
+            <Link to="/about">
               <button className="font-roboto flex items-center justify-between w-[12rem] px-4 py-2 md:w-60 md:px-7 md:py-3 rounded-2xl bg-[#2D2862] text-xl  md:text-2xl text-white slider-button">
                 გაიგეთ მეტი
                 <FaArrowRight />
               </button>
-            </a>
+            </Link>
           </div>
         </div>
       </SwiperSlide>
@@ -79,12 +80,12 @@ function Slider() {
             <p className="text-lg md:text-2xl text-[#FDE047] mt-2 font-roboto">
               ახლა რეგისტრაცია ჩვენი საიტიდან შეგიძლიათ!
             </p>
-            <a href="/register">
+            <Link to="/register">
               <button className="font-roboto flex items-center justify-between w-[12rem] px-4 py-2 md:w-60 md:px-6 md:py-3 rounded-2xl bg-[#2D2862] md:text-2xl text-white slider-button">
                 რეგისტრაცია
                 <FaPenToSquare />
               </button>
-            </a>
+            </Link>
           </div>
         </div>
       </SwiperSlide>
