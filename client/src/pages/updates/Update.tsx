69e4ece07dbf3a712eccf1d24468e13769b79a91
User <Ubitoz133@gmail.com>
2024-07-25 14:51:32 +0400

    added updates page

---
+++ client/src/pages/updates/Update.tsx
@@ -0,0 +1,11 @@
+const Updates = () => {
+  return (
+    <div>
+      <h1 className="mt-60 mb-48 text-5xl text-center">
+        მიმდინარეობს ტექნიკური სამუშაოები
+      </h1>
+    </div>
+  );
+};
+
+export default Updates;
