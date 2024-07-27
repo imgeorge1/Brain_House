197f37b0bb6ac2d22780c7bb17e2eae4a7268867
User <Ubitoz133@gmail.com>
2024-07-27 13:02:55 +0400

    added responsiveness to payment.tsx

--- client/src/pages/payment/Payment.tsx
+++ client/src/pages/payment/Payment.tsx
@@ -1,6 +1,6 @@
 const Payment = () => {
   return (
-    <div className="w-1/3 mx-auto mb-24 mt-40 ">
+    <div className="w-[100%] md:w-1/2 lg:w-[40%] mx-auto mb-24 mt-40 ">
       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8">
         <div className="mb-6">
           <label
@@ -9,22 +9,22 @@ const Payment = () => {
           >
             ანგარიშის ნომერი
           </label>
-          <div className="flex items-center justify-start pl-2 gap-2  ">
-            <span style={{ fontSize: "15px" }}>TBC ბანკი</span>{" "}
+          <div className="sm:flex items-center justify-start pl-2 gap-2 ">
+            <span style={{ fontSize: "15px" }}>TBC ბანკი</span>
             <input
               type="text"
               id="iban"
-              className="shadow appearance-none border rounded w-9/12 py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded sm:w-9/12 py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
               readOnly
               value="GE26TB7765836020100010"
             />
           </div>
-          <div className="flex items-center justify-start gap-5 pl-2 mt-2 ">
+          <div className="sm:flex items-center justify-start gap-5 pl-2 mt-2 ">
             <span style={{ fontSize: "15px" }}>მიმღები</span>{" "}
             <input
               type="text"
               id="recipient"
-              className="shadow appearance-none border rounded w-9/12 py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded sm:w-9/12 py-2 px-3 pl-2 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
               readOnly
               value="შ.პ.ს. ბრეინ ჰაუსი 2021"
             />
