d07c225faa3c8ab742759c5a7462facf1b871640
Anri Shvangiradze <shvangiradze22giorgi@gmail.com>
2025-03-27 16:20:07 +0400

    ..

--- client/src/utils/API.ts
+++ client/src/utils/API.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const DEV_MODE = "production";
 console.log(DEV_MODE);
 console.log(
-  DEV_MODE ? "http://localhost:3000" : "https://brainhouse.onrender.com"
+  !DEV_MODE ? "http://localhost:3000" : "https://brainhouse.onrender.com"
 );
 
 const API = axios.create({
