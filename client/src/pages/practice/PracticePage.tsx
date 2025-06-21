3e5870016957d6ff1535b95fde043ad6e0340589
imgeorge1 <127306696+imgeorge1@users.noreply.github.com>
2025-06-21 23:17:24 +0400

    Update PracticePage.tsx

--- client/src/pages/practice/PracticePage.tsx
+++ client/src/pages/practice/PracticePage.tsx
@@ -1,20 +1,20 @@
 import Practice from "../../components/practice/Practice";
-import { useUserContext } from "../../context/UserContext";
+// import { useUserContext } from "../../context/UserContext";
 
 const PracticePage = () => {
-  const { currentUser, booleanPaid } = useUserContext();
+  // const { currentUser, booleanPaid } = useUserContext();
   return (
     <>
-      {(currentUser?.email === "beka.lomsadze.1@btu.edu.ge" && booleanPaid) ||
+        <Practice />
+{/*       {(currentUser?.email === "beka.lomsadze.1@btu.edu.ge" && booleanPaid) ||
       (currentUser?.email === "shvangiradze22giorgi@gmail.com" &&
         booleanPaid) ||
       (currentUser?.email === "ubitoz133@gmail.com" && booleanPaid) ? (
-        <Practice />
       ) : (
         <p className="text-3xl my-80 text-red-600 font-bold text-center">
           პრაქტიკა გამოჩნდება ფასიანი პაკეტის შეძენის შემდეგ
         </p>
-      )}
+      )} */}
     </>
   );
 };
