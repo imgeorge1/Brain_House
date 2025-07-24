1fbd3b0981bb03cb6aef6ac0b78748605164cdd6
imgeorge1 <127306696+imgeorge1@users.noreply.github.com>
2025-07-24 20:54:36 +0400

    Update categoryData.ts

--- client/src/data/categoryData.ts
+++ client/src/data/categoryData.ts
@@ -215,13 +215,13 @@ const categoryData2 = [
     videoUrl:
       "https://drive.google.com/file/d/17j6HEaqQrSYstrw3t8z0IwYFsIGCH57n/preview",
   },
-  {
-    id: 30,
-    category: "მოძრაობის უსაფრთხოება",
-    index: 32,
-    videoUrl:
-      "https://drive.google.com/file/d/1wZT72ejlk1UZMzt7CDGhyS0MwZVBwuYI/preview https://drive.google.com/file/d/18CwmFhlG-VFWryb8fqV2srN4o6S_On_H/preview",
-  },
+//  {
+ //   id: 30,
+ //   category: "მოძრაობის უსაფრთხოება",
+ //   index: 32,
+//    videoUrl:
+//      "https://drive.google.com/file/d/1wZT72ejlk1UZMzt7CDGhyS0MwZVBwuYI/preview https://drive.google.com/file/d/18CwmFhlG-VFWryb8fqV2srN4o6S_On_H/preview",
+//  },
 ];
 
 export default categoryData2;
