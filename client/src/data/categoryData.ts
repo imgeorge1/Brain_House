08e402d515d72cb04aedee7cfcb8601c7a6965ca
imgeorge1 <127306696+imgeorge1@users.noreply.github.com>
2025-08-07 21:06:15 +0400

    Update categoryData.ts

--- client/src/data/categoryData.ts
+++ client/src/data/categoryData.ts
@@ -118,46 +118,46 @@ const categoryData2 = [
     videoUrl: "https://drive.google.com/file/d/1mlA1bditMZ-rJgxtjOtVNs7t_XmBYy-P/preview",
   },
   {
+    id: 1,
+    category: "მძღოლი, მგზავრი და ქვეითი, ნიშნები, კონვეცია",
+    index: 18,
+    videoUrl:
+      "https://drive.google.com/file/d/1FqLBpM8TbskDMTHHxC2NwEleQP5W2Yjq/preview",
+  },
+  {
     id: 5,
     category: "ამკრძალავი ნიშნები",
-    index: 18,
+    index: 19,
     videoUrl:
       "https://drive.google.com/file/d/1XPa6Qw_1JmiSBMSPwAq6RAO1Cmwcqomc/preview",
   },
   {
     id: 6,
     category: "მიმთითებელი ნიშნები",
-    index: 19,
+    index: 20,
     videoUrl:
       "https://drive.google.com/file/d/1vvDClkf1DXuL1OizW8f-SXz2Kl-bt1XE/preview",
   },
   {
     id: 12,
     category: "სპეციალური სიგნალის გამოყენება",
-    index: 20,
+    index: 21,
     videoUrl:
       "https://drive.google.com/file/d/1fGvyLtXDp0E09tQ6qsjnzPv1t6MFEIIf/preview",
   },
   {
     id: 13,
     category: "საავარიო შუქური სიგნალიზაცია",
-    index: 21,
+    index: 22,
     videoUrl:
       "https://drive.google.com/file/d/1EeniWZCHqaVQ3QyciSa_xPRjHAc3X9_-/preview",
   },
   {
     id: 9,
     category: "დამატებითი ინფორმაციის ნიშნები",
-    index: 22,
-    videoUrl:
-      "https://drive.google.com/file/d/1CgpIc0lZLDTIkPrRKtUruDnNFHdizvw_/preview",
-  },
-  {
-    id: 1,
-    category: "მძღოლი, მგზავრი და ქვეითი, ნიშნები, კონვეცია",
     index: 23,
     videoUrl:
-      "https://drive.google.com/file/d/1FqLBpM8TbskDMTHHxC2NwEleQP5W2Yjq/preview",
+      "https://drive.google.com/file/d/1CgpIc0lZLDTIkPrRKtUruDnNFHdizvw_/preview",
   },
   {
     id: 14,
@@ -166,62 +166,62 @@ const categoryData2 = [
     videoUrl:
       "https://drive.google.com/file/d/1kX-vv_s5WfuY6Frx3JAY9LWO6JN46Rrf/preview",
   },
-    {
-   id: 30,
-    category: "მოძრაობის უსაფრთხოება",
-  index: 25,
-   videoUrl:
-     "https://drive.google.com/file/d/1wZT72ejlk1UZMzt7CDGhyS0MwZVBwuYI/preview https://drive.google.com/file/d/18CwmFhlG-VFWryb8fqV2srN4o6S_On_H/preview",
- },
   {
     id: 31,
     category: "ადმინისტრაციული კანონი",
-    index: 26,
+    index: 25,
     videoUrl:
       "https://drive.google.com/file/d/1NTioStNbM4RrJMZXAy0BOC5sSBu7fSdT/preview",
   },
   {
     id: 29,
     category: "სამედიცინო დახმარება",
-    index: 27,
+    index: 26,
     videoUrl:
       "https://drive.google.com/file/d/15XKo1iU7ag5PSvwR5FFuZBHrSZhLVnIA/preview",
   },
   {
     id: 18,
     category: "სამუხრუჭე მანძილი, დისტანცია",
-    index: 28,
+    index: 27,
     videoUrl:
       "https://drive.google.com/file/d/19-UVBDzH73QY_SpSUfYAoucPOi7yRn_F/preview",
   },
   {
     id: 20,
     category: "გზაჯვარედინის გავლა",
-    index: 29,
+    index: 28,
     videoUrl:
       "https://drive.google.com/file/d/1qVHsMBD8PdjchZHqy4eIo5uDHXgh-JBX/preview",
   },
   {
     id: 10,
     category: "შუქნიშნის სიგნალები",
-    index: 30,
+    index: 29,
     videoUrl:
       "https://drive.google.com/file/d/1PVHsyIQ16D9Vm1dcwmarqPr9ygz58P7Q/preview",
   },
   {
     id: 11,
     category: "მარეგულირებლის სიგნალები",
-    index: 31,
+    index: 30,
     videoUrl:
       "https://drive.google.com/file/d/17gvFmO5cLmiSS_2GsFR2k_7cxUZzqxS4/preview",
   },
   {
     id: 32,
     category: "ეკო-მართვა",
-    index: 32,
+    index: 31,
     videoUrl:
       "https://drive.google.com/file/d/17j6HEaqQrSYstrw3t8z0IwYFsIGCH57n/preview",
   },
+  {
+    id: 30,
+    category: "მოძრაობის უსაფრთხოება",
+    index: 32,
+    videoUrl:
+     "https://drive.google.com/file/d/1wZT72ejlk1UZMzt7CDGhyS0MwZVBwuYI/preview https://drive.google.com/file/d/18CwmFhlG-VFWryb8fqV2srN4o6S_On_H/preview",
+ },
 ];
 
 export default categoryData2;
