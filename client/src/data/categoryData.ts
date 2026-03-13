2134560c2cfd945e9966db0aa1e179acf1cabe2b
Giorgi <127306696+imgeorge1@users.noreply.github.com>
2026-03-13 19:29:34 +0400

    Update categoryData.ts

--- client/src/data/categoryData.ts
+++ client/src/data/categoryData.ts
@@ -37,64 +37,85 @@ const categoryData2 = [
       "https://drive.google.com/file/d/1_AH1cz2S54xp2hzInM28ew7OPfaYjmMk/preview",
   },
   {
+    id: 30,
+    category: "მოძრაობის უსაფრთხოება",
+    index: 6,
+    videoUrl:
+     "https://drive.google.com/file/d/1wZT72ejlk1UZMzt7CDGhyS0MwZVBwuYI/preview https://drive.google.com/file/d/18CwmFhlG-VFWryb8fqV2srN4o6S_On_H/preview",
+  },
+  {
+    id: 1,
+    category: "მძღოლი, მგზავრი და ქვეითი, ნიშნები, კონვეცია",
+    index: 7,
+    videoUrl:
+      "https://drive.google.com/file/d/1FqLBpM8TbskDMTHHxC2NwEleQP5W2Yjq/preview",
+  },
+  {
+    id: 5,
+    category: "ამკრძალავი ნიშნები",
+    index: 8,
+    videoUrl:
+      "https://drive.google.com/file/d/1XPa6Qw_1JmiSBMSPwAq6RAO1Cmwcqomc/preview",
+  },
+  {
     id: 8,
     category: "სერვისის ნიშნები",
-    index: 6,
+    index: 9,
     videoUrl:
       "https://drive.google.com/file/d/1zEcExd2izh80rIGxf5IrwTbQeWJhHLze/preview",
   },
   {
     id: 2,
     category: "უწესივრობა და მართვის პირობები",
-    index: 7,
+    index: 10,
     videoUrl:
       "https://drive.google.com/file/d/1JdkbINAHiRr7WeO6OHNPq2TVdk5vNukV/preview",
   },
   {
     id: 7,
     category: "საინფორმაციო-მაჩვენებელი ნიშნები",
-    index: 8,
+    index: 11,
     videoUrl:
       "https://drive.google.com/file/d/1Y0rfr3VRIO3tKgOn9Y3ZbheMWd2zeCnK/preview",
   },
   {
     id: 27,
     category: "ველოსიპედი, მოპედი და პირუტყვის გადარეკვა",
-    index: 9,
+    index: 12,
     videoUrl:
       "https://drive.google.com/file/d/1DO_HUubUFv6s5wEXT94u8bEVngD9dR4x/preview",
   },
   {
     id: 28,
     category: "საგზაო მონიშვნა",
-    index: 10,
+    index: 13,
     videoUrl:
       "https://drive.google.com/file/d/1wt_0C7CDQMv3IcR_uMTTLqNmVKkpQ5N7/preview",
   },
   {
     id: 15,
     category: "მოძრაობა, მანევრირება, სავალი ნაწილი",
-    index: 11,
+    index: 14,
     videoUrl:
       "https://drive.google.com/file/d/1LNKTQXaqub25gWkHqn3TrdIf82PcK-92/preview",
   }, 
   {
     id: 26,
     category: "გადაზიდვები, ხალხი, ტვირთი",
-    index: 12,
+    index: 15,
     videoUrl: "https://drive.google.com/file/d/1IKNRL9i5afleq_HAmhqAP0mbRiOiw7qh/preview",
   },
   {
     id: 19,
     category: "გაჩერება დგომა",
-    index: 13,
+    index: 16,
     videoUrl:
       "https://drive.google.com/file/d/1HOJygatWf8bV0pjKY5L6SwXYq0QGUEmb/preview",
   },
   {
     id: 17,
     category: "მოძრაობის სიჩქარე",
-    index: 14,
+    index: 17,
     videoUrl: "https://drive.google.com/file/d/1eUwYGyzFeCWXhvCUTIIOb6eN9eTjB4sF/preview",
   },
   // {
@@ -107,7 +128,7 @@ const categoryData2 = [
   {
     id: 16,
     category: "გასწრება შემხვედრის გვერდის ავლით",
-    index: 15,
+    index: 18,
     videoUrl:
       "https://drive.google.com/file/d/1vYWtraZ_0Iqc1GOrgjKeNJPmtQmIqIFa/preview",
   },
@@ -115,113 +136,92 @@ const categoryData2 = [
   {
     id: 3,
     category: "მაფრთხილებელი ნიშნები",
-    index: 16,
+    index: 19,
     videoUrl:
       "https://drive.google.com/file/d/1UugO30-4VeJRVJq37EzVpmeEteGzGM3y/preview",
   },
   {
     id: 4,
     category: "პრიორიტეტის ნიშნები",
-    index: 17,
+    index: 20,
     videoUrl: "https://drive.google.com/file/d/1mlA1bditMZ-rJgxtjOtVNs7t_XmBYy-P/preview",
   },
   {
     id: 6,
     category: "მიმთითებელი ნიშნები",
-    index: 18,
+    index: 21,
     videoUrl:
       "https://drive.google.com/file/d/1vvDClkf1DXuL1OizW8f-SXz2Kl-bt1XE/preview",
   },
   {
     id: 12,
     category: "სპეციალური სიგნალის გამოყენება",
-    index: 19,
+    index: 22,
     videoUrl:
       "https://drive.google.com/file/d/1fGvyLtXDp0E09tQ6qsjnzPv1t6MFEIIf/preview",
   },
   {
     id: 13,
     category: "საავარიო შუქური სიგნალიზაცია",
-    index: 20,
+    index: 23,
     videoUrl:
       "https://drive.google.com/file/d/1EeniWZCHqaVQ3QyciSa_xPRjHAc3X9_-/preview",
   },
   {
     id: 9,
     category: "დამატებითი ინფორმაციის ნიშნები",
-    index: 21,
+    index: 24,
     videoUrl:
       "https://drive.google.com/file/d/1CgpIc0lZLDTIkPrRKtUruDnNFHdizvw_/preview",
   },
   {
     id: 14,
     category: "სანათი ხელსაწყოები, ხმოვანი სიგნალი",
-    index: 22,
+    index: 25,
     videoUrl:
       "https://drive.google.com/file/d/1kX-vv_s5WfuY6Frx3JAY9LWO6JN46Rrf/preview",
   },
   {
     id: 31,
     category: "ადმინისტრაციული კანონი",
-    index: 23,
+    index: 26,
     videoUrl:
       "https://drive.google.com/file/d/1NTioStNbM4RrJMZXAy0BOC5sSBu7fSdT/preview",
   },
   {
     id: 29,
     category: "სამედიცინო დახმარება",
-    index: 24,
+    index: 27,
     videoUrl:
       "https://drive.google.com/file/d/15XKo1iU7ag5PSvwR5FFuZBHrSZhLVnIA/preview",
   },
   {
     id: 18,
     category: "სამუხრუჭე მანძილი, დისტანცია",
-    index: 25,
+    index: 28,
     videoUrl:
       "https://drive.google.com/file/d/19-UVBDzH73QY_SpSUfYAoucPOi7yRn_F/preview",
   },
   {
     id: 20,
     category: "გზაჯვარედინის გავლა",
-    index: 26,
+    index: 29,
     videoUrl:
       "https://drive.google.com/file/d/1qVHsMBD8PdjchZHqy4eIo5uDHXgh-JBX/preview",
   },
   {
     id: 10,
     category: "შუქნიშნის სიგნალები",
-    index: 27,
+    index: 30,
     videoUrl:
       "https://drive.google.com/file/d/1PVHsyIQ16D9Vm1dcwmarqPr9ygz58P7Q/preview",
   },
   {
     id: 11,
     category: "მარეგულირებლის სიგნალები",
-    index: 28,
-    videoUrl:
-      "https://drive.google.com/file/d/17gvFmO5cLmiSS_2GsFR2k_7cxUZzqxS4/preview",
-  },
-  {
-    id: 30,
-    category: "მოძრაობის უსაფრთხოება",
-    index: 29,
-    videoUrl:
-     "https://drive.google.com/file/d/1wZT72ejlk1UZMzt7CDGhyS0MwZVBwuYI/preview https://drive.google.com/file/d/18CwmFhlG-VFWryb8fqV2srN4o6S_On_H/preview",
-  },
-  {
-    id: 1,
-    category: "მძღოლი, მგზავრი და ქვეითი, ნიშნები, კონვეცია",
-    index: 30,
-    videoUrl:
-      "https://drive.google.com/file/d/1FqLBpM8TbskDMTHHxC2NwEleQP5W2Yjq/preview",
-  },
-  {
-    id: 5,
-    category: "ამკრძალავი ნიშნები",
     index: 31,
     videoUrl:
-      "https://drive.google.com/file/d/1XPa6Qw_1JmiSBMSPwAq6RAO1Cmwcqomc/preview",
+      "https://drive.google.com/file/d/17gvFmO5cLmiSS_2GsFR2k_7cxUZzqxS4/preview",
   },  
   {
     id: 32,
