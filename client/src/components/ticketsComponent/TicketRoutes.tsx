f4cf87f5bd70c32bbc081334c885f8fbcfe696d6
Giorgi <127306696+imgeorge1@users.noreply.github.com>
2026-08-03 00:54:25 +0400

    Update TicketRoutes.tsx

--- client/src/components/ticketsComponent/TicketRoutes.tsx
+++ client/src/components/ticketsComponent/TicketRoutes.tsx
@@ -25,7 +25,7 @@ const TicketRoutes = () => {
         <motion.div
           initial={{ opacity: 0, y: -200 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 0.5 }}
+          transition={{ duration: 0.4, delay: 0.2 }}
           className="drivebtnparent"
         >
           <NavLink
@@ -103,7 +103,7 @@ const TicketRoutes = () => {
                   key={item.id}
                   initial={{ opacity: 0, x: -200 }}
                   animate={{ opacity: 1, x: 0 }}
-                  transition={{ duration: 0.8, delay: 0.1 * item.index }}
+                  transition={{ duration: 0.4, delay: 0.1 * item.index }}
                   onClick={() => {
                     if ((isPaid || isFirstItem) && isCompleted && !isLocked) {
                       handleChooseCategory(item.category);
