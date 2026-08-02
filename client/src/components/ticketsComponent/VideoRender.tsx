263e18e54160d275366a6bd258964358be487e38
Giorgi <127306696+imgeorge1@users.noreply.github.com>
2026-08-03 02:39:59 +0400

    Update VideoRender.tsx

--- client/src/components/ticketsComponent/VideoRender.tsx
+++ client/src/components/ticketsComponent/VideoRender.tsx
@@ -20,7 +20,7 @@ const VideoRender = ({
     return videoUrls.map((url, index) => (
         <div className="w-full max-w-4xl mx-auto h-[450px] overflow-hidden">
       <iframe
-        className="w-full h-[680px] md:h-[460px] -translate-y-[230px] "
+        className="w-full h-[680px] md:h-[460px] -translate-y-[210px] "
         key={`video-${checkForVideo.id}-${index}`} // Unique key for each iframe
         title={`Video ${checkForVideo.id}-${index}`}
         src={url}
@@ -35,7 +35,7 @@ const VideoRender = ({
       <div>
         <div className="w-full max-w-4xl mx-auto h-[450px] overflow-hidden">
         <iframe
-          className="w-full h-[640px] md:h-[480px] -translate-y-[230px]" 
+          className="w-full h-[640px] md:h-[480px] -translate-y-[210px]" 
           key={`video-${checkForVideo.id}`} // Unique key for each iframe
           title={`Video ${checkForVideo.id}`}
           src={videoUrls[0]}
