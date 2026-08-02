643b52636e024223101a8670aaa5e44a11765f80
Giorgi <127306696+imgeorge1@users.noreply.github.com>
2026-08-03 01:55:48 +0400

    Update RoadSign.tsx

--- client/src/components/roadSign/RoadSign.tsx
+++ client/src/components/roadSign/RoadSign.tsx
@@ -29,16 +29,16 @@ const RoadSign = () => {
           <p className="px-2">დაბრუნება</p>
         </Link>
       </button>
-        <iframe
-          className="w-full max-w-4xl mx-auto h-[280px] md:h-[460px] my-2 border mb-10
-          border-slate-300 border-solid rounded-sm"
-          title="Video for sign in"
-          src={
-            "https://drive.google.com/file/d/1inPqi124ACuYemMz3ZUL8MU1FxU77OZo/preview"
-          }
-          sandbox="allow-same-origin allow-scripts"
-          alloFullScreen
-        />
+      <iframe 
+        className="w-full max-w-4xl mx-auto h-[280px] md:h-[460px] my-2 border border-slate-300 
+        border-solid rounded-sm" 
+        title="Video for sign in"
+        src={
+          "https://drive.google.com/file/d/1inPqi124ACuYemMz3ZUL8MU1FxU77OZo/preview"
+        }
+        sandbox="allow-same-origin allow-scripts" 
+        allowFullScreen 
+      />
       <ul className="flex flex-wrap mx-auto justify-center gap-2 max-w-[950px] mb-20">
         {roadSignsData.map((signs) => (
           <motion.li
