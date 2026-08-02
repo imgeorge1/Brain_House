c2fa72867b7020a9694a7ca5f84d856790b1b68c
Giorgi <127306696+imgeorge1@users.noreply.github.com>
2026-08-03 02:27:24 +0400

    Update VideoRender.tsx

--- client/src/components/ticketsComponent/VideoRender.tsx
+++ client/src/components/ticketsComponent/VideoRender.tsx
@@ -18,26 +18,31 @@ const VideoRender = ({
 
   if (isThirtyPath) {
     return videoUrls.map((url, index) => (
+        <div className="w-full max-w-4xl mx-auto h-[280px] overflow-hidden">
       <iframe
-        className="w-full h-[680px] md:h-[460px] py-4 "
+        className="w-full h-[680px] md:h-[460px] py-4 -translate-x-[2.5%] -translate-y-[20px] "
         key={`video-${checkForVideo.id}-${index}`} // Unique key for each iframe
         title={`Video ${checkForVideo.id}-${index}`}
         src={url}
         sandbox="allow-same-origin allow-scripts"
         allowFullScreen
       />
+      
+        </div>
     ));
   } else {
     return (
       <div>
+        <div className="w-full max-w-4xl mx-auto h-[280px] overflow-hidden">
         <iframe
-          className="w-full h-[640px] md:h-[380px] my-4"
+          className="w-full h-[640px] md:h-[380px] my-4 -translate-x-[2.5%] -translate-y-[20px]" 
           key={`video-${checkForVideo.id}`} // Unique key for each iframe
           title={`Video ${checkForVideo.id}`}
           src={videoUrls[0]}
           sandbox="allow-same-origin allow-scripts"
           allowFullScreen
         />
+        </div>
         <div className="flex justify-between pt-2 mx-2">
           <p className="flex justify-center items-center">
             <img
